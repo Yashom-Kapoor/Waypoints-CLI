2a35ed901d9bdb6f9ea31016fd91ecdffbd8cd21
Yashom Kapoor <yashom.kapoor@gmail.com>
2026-05-13 18:21:03 -0700

    included tmp dir for install

--- install.sh
+++ install.sh
@@ -2,13 +2,17 @@
 
 echo "Installing Waypoints CLI..."
 
+# Make temp dir to install git repo
+TMP_DIR=$(mktemp -d)
+git clone https://github.com/Yashom-Kapoor/Waypoints-CLI "$TMP_DIR"
+
 # Create local bin, data, and shell folder if needed
 mkdir -p ~/.waypoints-cli/bin
 mkdir -p ~/.waypoints-cli/data
-mkdir -p ~/.waypoints-cli/shell/waypoints.sh
+mkdir -p ~/.waypoints-cli/shell
 
 # Copy binary to bin
-cp waypoints-cli ~/.waypoints-cli/bin/waypoints-cli
+cp "$TMP_DIR/waypoints-cli" ~/.waypoints-cli/bin/waypoints-cli
 chmod +x ~/.waypoints-cli/bin/waypoints-cli
 
 # Initialize waypoints.json
@@ -17,10 +21,10 @@ if [ ! -f ~/.waypoints-cli/data/waypoints.json ]; then
 fi
 
 # Copy shell
-cp waypoints.sh ~/.waypoints-cli/shell/waypoints.sh
+cp "$TMP_DIR/waypoints.sh" ~/.waypoints-cli/shell/waypoints.sh
 
 # Copy uninstall
-cp uninstall.sh ~/.waypoints-cli/uninstall.sh
+cp "$TMP_DIR/uninstall.sh" ~/.waypoints-cli/uninstall.sh
 
 # Add source line if not there already in zshrc
 if ! grep -q "waypoints.sh" ~/.zshrc; then
