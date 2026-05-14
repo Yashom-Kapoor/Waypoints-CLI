039e886da6090e5b515ac68542b753e995d88a7d
Yashom Kapoor <yashom.kapoor@gmail.com>
2026-05-13 23:54:45 -0700

    added context aware completion

--- waypoints.sh
+++ waypoints.sh
@@ -3,7 +3,7 @@ wp() {
     cmd="$1" # Capture first argument
 
     # Handle uninstall
-    if [ "$cmd" = "uninstall" ] || [ "$cmd" = "add" ]; then
+    if [[ "$cmd" = "uninstall" || "$cmd" = "add" ]]; then
         ~/.waypoints-cli/bin/waypoints-cli "$@"
     
     else 
@@ -13,7 +13,7 @@ wp() {
         exit_code=$? # Exit cocde to determine cd or not
 
         # -d => if directory exists
-        if [ $exit_code -eq 0 ] && [ -d "$result" ]; then
+        if [[ $exit_code -eq 0 && -d "$result" ]]; then
             cd "$result"
         else
             echo "$result"
@@ -24,9 +24,22 @@ wp() {
 # Completion
 _wp() {
     local completions
+    local command
+
+    command="${words[2]}"
+
+    # add for <path>
+    if [[ "$command" == "add" && $CURRENT -eq 3 ]]; then
+        _path_files
+        return
+    fi
+
+    if [[ "$command" == "add" && $CURRENT -ge 4 ]]; then
+        return
+    fi
 
     completions=("${(@f)$(
-        ~/.waypoints/bin/waypoints-cli __complete "${words[CURRENT]}"
+        ~/.waypoints/bin/waypoints-cli __complete "$command" "${words[CURRENT]}"
     )}")
 
     compadd -a completions
