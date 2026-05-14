f9cf41dcb9e6aa13484815479194cb40ccf2fc95
Yashom Kapoor <yashom.kapoor@gmail.com>
2026-05-13 19:31:21 -0700

    pass uninstall or add to binary

--- waypoints.sh
+++ waypoints.sh
@@ -1,14 +1,21 @@
 # Define function/command wp
 wp() {
-    # Set result to output of waypoints-cli
-    # Pass all arguments to waypoints-cli ($@)
-    result=$(~/.waypoints-cli/bin/waypoints-cli "$@")
-    exit_code=$?
+    cmd="$1" # Capture first argument
 
-    # -d => if directory exists
-    if [ $status -eq 0 ] && [ -d "$result" ]; then
-        cd "$result"
-    else
-        echo "$result"
-    fi #End of if function
+    # Handle uninstall
+    if [ "$cmd" = "uninstall" ] || [ "$cmd" = "add" ]; then
+        ~/.waypoints-cli/bin/waypoints-cli "$@"
+    
+    else 
+        # Set result to output of waypoints-cli
+        # Pass all arguments to waypoints-cli ($@)
+        result=$(~/.waypoints-cli/bin/waypoints-cli "$@")
+        exit_code=$? # Exit cocde to determine cd or not
+
+        # -d => if directory exists
+        if [ $status -eq 0 ] && [ -d "$result" ]; then
+            cd "$result"
+        else
+            echo "$result"
+        fi #End of if function
 }
